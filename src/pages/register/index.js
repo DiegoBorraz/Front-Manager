9bb0816a809136a1ff52cdbd06b10669b4288037
Diego Avila <diegoavila@americansbitcoin.com>
2020-04-07 02:20:56 -0300

    Feito a parte do front de registro

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,57 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import "moment/locale/pt-br";
 import moment from "moment";
-import { Formik, Form, Field } from "formik";
-import { Button, LinearProgress } from "@material-ui/core";
+import { Formik, Field } from "formik";
+import { Button } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
-import { TimePicker, DatePicker, DateTimePicker } from "formik-material-ui-pickers";
+import { DatePicker } from "formik-material-ui-pickers";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment"; // choose your lib
+import * as Yup from "yup";
 
-import { textNumber, checkMail } from "../../util";
-
-const validate = (values) => {
-  const errors = {};
-
-  console.log("error = ", errors);
-  if (!values.name) {
-    errors.name = "Nome deve ser informado.";
-  } else if (values.name.length < 2) {
-    errors.name = "Nome deve ter no mínimo 2 caracteres.";
-  }
-  if (!values.email) {
-    errors.email = "Email deve ser informado.";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Email invalido.";
-  }
-  if (!values.dateOfBirth) {
-    errors.dateOfBirth = "Data de nascimento deve ser informada.";
-  } else if (values.dateOfBirth) {
-    var difTime = moment(values.dateOfBirth).diff(new Date());
-    var duration = moment.duration(difTime);
-    console.log("DURAÇÃO DIAS == ", duration.years());
-    if (duration.years() > -18) {
-      errors.dateOfBirth = "Você deve ter no mínimo 18 anos.";
-    }
-  }
-  if (!values.password) {
-    errors.password = "Senha deve ser digitado.";
-  } else if (values.password.length < 6) {
-    errors.password = "Senha deve ter no mínimo 6 caracteres.";
-  }
-  if (!values.repeatPassword) {
-    errors.repeatPassword = "Senha de confirmação deve ser digitada.";
-  } else if (values.repeatPassword != values.password) {
-    errors.repeatPassword = "Sua senha e senha de confirmação devem ser iguais.";
-  }
-  return errors;
+const validate = () => {
+  return Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Nome deve ter no mínimo 2 caracteres.")
+      .max(50, "Nome deve ter no máximo 50 caracteres.")
+      .required("Nome deve ser informado."),
+    email: Yup.string().email("Email invalido.").required("Email deve ser informado."),
+    dateOfBirth: Yup.date()
+      .max(moment().subtract(18, "years"), "Você deve ter no mínimo 18 anos.")
+      .required("Data deve ser informada"),
+    password: Yup.string()
+      .min(6, "Senha deve ter no mínimo 6 caracteres.")
+      .max(50, "Senha deve ter no máximo 50 caracteres.")
+      .required("Senha deve ser informada"),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "As senhas devem corresponder")
+      .min(6, "Senha deve ter no mínimo 6 caracteres.")
+      .required("Senha deve ser informada"),
+  });
 };
 
 const onSubmit = (values, { setSubmitting }) => {
-  console.log("values = ", values);
-  console.log("setSubmitting =", setSubmitting);
+  console.log(values);
   setTimeout(() => {
     setSubmitting(false);
     alert(JSON.stringify(values, null, 2));
@@ -60,45 +44,63 @@ const onSubmit = (values, { setSubmitting }) => {
 
 const Register = () => {
   const styles = useStyles();
-  const [state, setState] = useState({
+  const state = {
     name: "",
     email: "",
-    dateOfBirth: new Date(),
+    dateOfBirth: new Date(moment().subtract(18, "years")),
     password: "",
     repeatPassword: "",
-  });
+  };
 
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <Grid container direction="row" justify="center" alignItems="center">
-        <Formik initialValues={state} validate={validate} onSubmit={onSubmit}>
-          {({ submitForm, isSubmitting }) => (
-            <form>
-              <Grid className={styles.textField}>
-                <Field component={TextField} name="name" label="Nome" type="text" />
-              </Grid>
-              <Grid className={styles.textField}>
-                <Field component={TextField} name="email" label="Email" type="email" />
-              </Grid>
-              <Grid className={styles.textField}>
-                <Field component={DatePicker} name="dateOfBirth" label="Data de nascimento" format="LL" />
-              </Grid>
-              <Grid className={styles.textField}>
-                <Field component={TextField} name="password" label="Senha" type="password" />
-              </Grid>
-              <Grid className={styles.textField}>
-                <Field component={TextField} name="repeatPassword" label="Confirme sua senha" type="password" />
-              </Grid>
-              <Grid className={styles.textField}>
-                <Button variant="contained" color="primary" disabled={isSubmitting} onClick={submitForm}>
-                  Submit
-                </Button>
-              </Grid>
-            </form>
-          )}
-        </Formik>
-      </Grid>
-    </MuiPickersUtilsProvider>
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Card className={styles.form}>
+        <CardContent>
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <Formik initialValues={state} validationSchema={validate} onSubmit={onSubmit}>
+              {({ submitForm, isSubmitting }) => (
+                <form>
+                  <Grid className={styles.textField}>
+                    <Field component={TextField} name="name" label="Nome" type="text" fullWidth />
+                  </Grid>
+                  <Grid className={styles.textField}>
+                    <Field component={TextField} name="email" label="Email" type="email" fullWidth />
+                  </Grid>
+                  <Grid className={styles.textField}>
+                    <Field
+                      component={DatePicker}
+                      name="dateOfBirth"
+                      label="Data de nascimento"
+                      format="LL"
+                      maxDate={new Date(moment().subtract(18, "years"))}
+                      minDate={new Date(moment().subtract(100, "years"))}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid className={styles.textField}>
+                    <Field component={TextField} name="password" label="Senha" type="password" fullWidth />
+                  </Grid>
+                  <Grid className={styles.textField}>
+                    <Field
+                      component={TextField}
+                      name="repeatPassword"
+                      label="Confirme sua senha"
+                      type="password"
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid className={styles.textField}>
+                    <Button variant="contained" color="primary" disabled={isSubmitting} onClick={submitForm} fullWidth>
+                      Cadastrar
+                    </Button>
+                  </Grid>
+                </form>
+              )}
+            </Formik>
+          </MuiPickersUtilsProvider>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
